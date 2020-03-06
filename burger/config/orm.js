cb37b6088119d656d6fe5aa53d5501bcedfd8e7f
GusGo <gus.gonzalez.15@gmail.com>
2020-03-06 14:06:46 -0500

    orm functions completed, not tested yet

--- burger/config/orm.js
+++ burger/config/orm.js
@@ -1,16 +1,30 @@
 const connection = require('./connection');
 
 const orm = {
-    selectAll: function () {
+    selectAll: function (cb) {
+        connection.query("SELECT * FROM burgers", (err, result) => {
+            if (err) throw err;
 
+            cb(result);
+        });
     },
 
-    insertOne: function () {
+    insertOne: function (burgerName, cb) {
+        let queryString = `INSERT INTO burgers (burger_name, devoured) VALUES (?, FALSE)`;
+        connection.query(queryString, burgerName, (err, result) => {
+            if (err) throw err;
 
+            cb(result);
+        });
     },
 
-    updateOne: function () {
+    updateOne: function (id, cb) {
+        let queryString = `UPDATE burgers SET devoured = FALSE WHERE id = ?`;
+        connection.query(queryString, id, (err, result) => {
+            if (err) throw err;
 
+            cb(result);
+        });
     }
 }
 
