ce1b43786750506ed01666e30160e0e35cb22d8a
GusGo <gus.gonzalez.15@gmail.com>
2020-03-06 16:55:40 -0500

    api put route

--- burger/controllers/burgers_controller.js
+++ burger/controllers/burgers_controller.js
@@ -15,9 +15,20 @@ router.get('/', (req, res) => {
 });
 
 router.post('/api/burgers', (req, res) => {
-    burger.addBurger(req.body.name, (result) => {
-        console.log("Burger Added Successfully!")
+    burger.addBurger(req.body.name, result => {
+        console.log("Burger Added Successfully!");
         res.json({ id: result.insertID });
+    });
+});
+
+router.put('/api/burgers/:id', (req, res) => {
+    burger.devourBurger(req.params.id, result => {
+        if (result.changedRows === 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        }
+        res.status(200).end();
+        console.log("Burger Devoured Successfully!");
     });
 });
 
