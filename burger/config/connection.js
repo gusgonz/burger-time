51bcbdf27784cfb9fa4e31be97aad090d82624eb
GusGo <gus.gonzalez.15@gmail.com>
2020-03-06 17:25:24 -0500

    re-config connection to be deployed to heroku

--- burger/config/connection.js
+++ burger/config/connection.js
@@ -1,12 +1,18 @@
 const mysql = require("mysql");
+var connection;
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "wwtwP1320",
-    database: "burger_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "wwtwP1320",
+        database: "burger_db"
+    });
+}
+
 
 connection.connect(err => {
     if (err) {
@@ -16,4 +22,5 @@ connection.connect(err => {
     console.log("connected as id " + connection.threadId);
 });
 
+connection.connect();
 module.exports = connection;
