29ed9e938ffadd85729f90655bfcfb87aebe7e35
GusGo <gus.gonzalez.15@gmail.com>
2020-03-06 13:41:27 -0500

    added get route to render html template file

--- burger/controllers/burgers_controller.js
+++ burger/controllers/burgers_controller.js
@@ -3,3 +3,14 @@ const burger = require('../models/burger');
 
 const router = express.Router();
 
+
+router.get('/', (req, res) => {
+
+
+    res.render('index');
+});
+
+
+
+
+module.exports = router;
