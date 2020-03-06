2bc7c3f7c710e60b4b1fc6ad8b132b5bea6044cb
GusGo <gus.gonzalez.15@gmail.com>
2020-03-06 16:00:22 -0500

    post api route to add burgers

--- burger/controllers/burgers_controller.js
+++ burger/controllers/burgers_controller.js
@@ -8,16 +8,17 @@ router.get('/', (req, res) => {
     burger.viewBurgers((result) => {
         console.log('View Burgers was Successful!');
 
-        let hbsObject = {
-            burgers: result
-        };
+        let hbsObject = { burgers: result };
 
         res.render('index', hbsObject);
     });
 });
 
-router.post('/', (req, res) => {
-
+router.post('/api/burger', (req, res) => {
+    burger.addBurger(req.body.name, (result) => {
+        console.log("Burger Added Successfully!")
+        res.json({ id: result.insertID });
+    });
 });
 
 
