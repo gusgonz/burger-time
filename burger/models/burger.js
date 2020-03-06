565e40b4939f5048bb8167eecab0545e427ec635
GusGo <gus.gonzalez.15@gmail.com>
2020-03-06 16:56:00 -0500

    devour burger funtion

--- burger/models/burger.js
+++ burger/models/burger.js
@@ -9,13 +9,17 @@ const burger = {
 
     addBurger: function (burgerName, cb) {
         orm.insertOne(burgerName, result => {
-            console.log(result)
+            // console.log(result)
             cb(result);
         });
     },
 
-    devourBurger: function () {
+    devourBurger: function (id, cb) {
+        orm.updateOne(id, result => {
+            console.log(result);
 
+            cb(result);
+        });
     }
 }
 
