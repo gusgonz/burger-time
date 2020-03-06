858c6154cb71b2a9b6a9ea850b4a82befa33f812
GusGo <gus.gonzalez.15@gmail.com>
2020-03-06 16:56:40 -0500

    devour button on click and ajax put request

--- burger/public/js/burgers.js
+++ burger/public/js/burgers.js
@@ -1,4 +1,17 @@
 $(() => {
+    $(".devour-button").on("click", function (event) {
+        let id = $(this).data("id");
+        console.log('id: ', id);
+
+        $.ajax(`api/burgers/${id}`, {
+            type: "PUT"
+        }).then(() => {
+            console.log("devoured burger");
+            // Reload the page to get the updated list
+            location.reload();
+        });
+    });
+
     $(".add-burger-form").on("submit", event => {
         event.preventDefault();
 
@@ -17,13 +30,6 @@ $(() => {
             }
         );
     });
-
-
-
-
-
-
-
 
 
 
